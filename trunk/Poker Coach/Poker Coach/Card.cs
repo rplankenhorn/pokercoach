ea3dca66a6389fceafd92ffea495257d74a8eff2
rplankenhorn <rplankenhorn@c5cdc586-8c4b-11dd-8ab4-67102e0efeef>
2008-09-30 02:56:59 +0000

    Template Uploaded.

---
+++ trunk/Poker Coach/Poker Coach/Card.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public enum SUITS
+{
+    CLUBS = 1,
+    DIAMONDS = 2,
+    HEARTS = 3,
+    SPADES = 4
+};
+
+public enum CARDVALUE
+{
+    ACE = 1,
+    TWO = 2,
+    THREE = 3,
+    FOUR = 4,
+    FIVE = 5,
+    SIX = 6,
+    SEVEN = 7,
+    EIGHT = 8,
+    NINE = 9,
+    TEN = 10,
+    JACK = 11,
+    QUEEN = 12,
+    KING = 13
+};
+
+namespace Poker_Coach
+{
+    public class Card
+    {
+        private SUITS suit;
+        private CARDVALUE value;
+
+        public Card(SUITS s, CARDVALUE v)
+        {
+            suit = s;
+            value = v;
+        }
+
+        public SUITS getSuit()
+        {
+            return suit;
+        }
+
+        public void setSuit(SUITS s)
+        {
+            suit = s;
+        }
+
+        public CARDVALUE getValue()
+        {
+            return value;
+        }
+
+        public void setValue(CARDVALUE v)
+        {
+            value = v;
+        }
+    }
+}
