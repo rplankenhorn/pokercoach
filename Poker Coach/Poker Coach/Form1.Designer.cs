386a4e1dc19bb5a69c60c0a7e5c715abdb327349
rplankenhorn <rplankenhorn@c5cdc586-8c4b-11dd-8ab4-67102e0efeef>
2008-09-30 03:13:22 +0000

    Added GUI updates.

--- Poker Coach/Poker Coach/Form1.Designer.cs
+++ Poker Coach/Poker Coach/Form1.Designer.cs
@@ -28,12 +28,441 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.comboBox6 = new System.Windows.Forms.ComboBox();
+            this.comboBox7 = new System.Windows.Forms.ComboBox();
+            this.comboBox8 = new System.Windows.Forms.ComboBox();
+            this.comboBox9 = new System.Windows.Forms.ComboBox();
+            this.comboBox10 = new System.Windows.Forms.ComboBox();
+            this.comboBox11 = new System.Windows.Forms.ComboBox();
+            this.comboBox12 = new System.Windows.Forms.ComboBox();
+            this.comboBox13 = new System.Windows.Forms.ComboBox();
+            this.comboBox14 = new System.Windows.Forms.ComboBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.cmdOppMinus = new System.Windows.Forms.Button();
+            this.cmdOppPlus = new System.Windows.Forms.Button();
+            this.txtNumOfOpponents = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox3);
+            this.groupBox1.Controls.Add(this.comboBox4);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 135);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(254, 81);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Your Cards";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "J",
+            "Q",
+            "K",
+            "A"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 37);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(37, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Text = "A";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Clubs",
+            "Diamonds",
+            "Hearts",
+            "Spades"});
+            this.comboBox2.Location = new System.Drawing.Point(55, 37);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(71, 21);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.Text = "Spades";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Clubs",
+            "Diamonds",
+            "Hearts",
+            "Spades"});
+            this.comboBox3.Location = new System.Drawing.Point(175, 37);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(71, 21);
+            this.comboBox3.TabIndex = 3;
+            this.comboBox3.Text = "Diamonds";
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "J",
+            "Q",
+            "K",
+            "A"});
+            this.comboBox4.Location = new System.Drawing.Point(132, 37);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(37, 21);
+            this.comboBox4.TabIndex = 2;
+            this.comboBox4.Text = "A";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.comboBox9);
+            this.groupBox2.Controls.Add(this.comboBox10);
+            this.groupBox2.Controls.Add(this.comboBox5);
+            this.groupBox2.Controls.Add(this.comboBox8);
+            this.groupBox2.Controls.Add(this.comboBox6);
+            this.groupBox2.Controls.Add(this.comboBox7);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(239, 100);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Flop";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.comboBox11);
+            this.groupBox3.Controls.Add(this.comboBox12);
+            this.groupBox3.Location = new System.Drawing.Point(257, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(84, 100);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Turn";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.comboBox13);
+            this.groupBox4.Controls.Add(this.comboBox14);
+            this.groupBox4.Location = new System.Drawing.Point(347, 12);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(84, 100);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "River";
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Items.AddRange(new object[] {
+            "Clubs",
+            "Diamonds",
+            "Hearts",
+            "Spades"});
+            this.comboBox5.Location = new System.Drawing.Point(82, 58);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(71, 21);
+            this.comboBox5.TabIndex = 7;
+            this.comboBox5.Text = "Diamonds";
+            // 
+            // comboBox6
+            // 
+            this.comboBox6.FormattingEnabled = true;
+            this.comboBox6.Items.AddRange(new object[] {
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "J",
+            "Q",
+            "K",
+            "A"});
+            this.comboBox6.Location = new System.Drawing.Point(79, 31);
+            this.comboBox6.Name = "comboBox6";
+            this.comboBox6.Size = new System.Drawing.Size(37, 21);
+            this.comboBox6.TabIndex = 6;
+            this.comboBox6.Text = "A";
+            // 
+            // comboBox7
+            // 
+            this.comboBox7.FormattingEnabled = true;
+            this.comboBox7.Items.AddRange(new object[] {
+            "Clubs",
+            "Diamonds",
+            "Hearts",
+            "Spades"});
+            this.comboBox7.Location = new System.Drawing.Point(6, 58);
+            this.comboBox7.Name = "comboBox7";
+            this.comboBox7.Size = new System.Drawing.Size(71, 21);
+            this.comboBox7.TabIndex = 5;
+            this.comboBox7.Text = "Spades";
+            // 
+            // comboBox8
+            // 
+            this.comboBox8.FormattingEnabled = true;
+            this.comboBox8.Items.AddRange(new object[] {
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "J",
+            "Q",
+            "K",
+            "A"});
+            this.comboBox8.Location = new System.Drawing.Point(6, 31);
+            this.comboBox8.Name = "comboBox8";
+            this.comboBox8.Size = new System.Drawing.Size(37, 21);
+            this.comboBox8.TabIndex = 4;
+            this.comboBox8.Text = "A";
+            // 
+            // comboBox9
+            // 
+            this.comboBox9.FormattingEnabled = true;
+            this.comboBox9.Items.AddRange(new object[] {
+            "Clubs",
+            "Diamonds",
+            "Hearts",
+            "Spades"});
+            this.comboBox9.Location = new System.Drawing.Point(162, 58);
+            this.comboBox9.Name = "comboBox9";
+            this.comboBox9.Size = new System.Drawing.Size(71, 21);
+            this.comboBox9.TabIndex = 9;
+            this.comboBox9.Text = "Diamonds";
+            // 
+            // comboBox10
+            // 
+            this.comboBox10.FormattingEnabled = true;
+            this.comboBox10.Items.AddRange(new object[] {
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "J",
+            "Q",
+            "K",
+            "A"});
+            this.comboBox10.Location = new System.Drawing.Point(159, 31);
+            this.comboBox10.Name = "comboBox10";
+            this.comboBox10.Size = new System.Drawing.Size(37, 21);
+            this.comboBox10.TabIndex = 8;
+            this.comboBox10.Text = "A";
+            // 
+            // comboBox11
+            // 
+            this.comboBox11.FormattingEnabled = true;
+            this.comboBox11.Items.AddRange(new object[] {
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "J",
+            "Q",
+            "K",
+            "A"});
+            this.comboBox11.Location = new System.Drawing.Point(6, 31);
+            this.comboBox11.Name = "comboBox11";
+            this.comboBox11.Size = new System.Drawing.Size(37, 21);
+            this.comboBox11.TabIndex = 10;
+            this.comboBox11.Text = "A";
+            // 
+            // comboBox12
+            // 
+            this.comboBox12.FormattingEnabled = true;
+            this.comboBox12.Items.AddRange(new object[] {
+            "Clubs",
+            "Diamonds",
+            "Hearts",
+            "Spades"});
+            this.comboBox12.Location = new System.Drawing.Point(6, 58);
+            this.comboBox12.Name = "comboBox12";
+            this.comboBox12.Size = new System.Drawing.Size(71, 21);
+            this.comboBox12.TabIndex = 11;
+            this.comboBox12.Text = "Spades";
+            // 
+            // comboBox13
+            // 
+            this.comboBox13.FormattingEnabled = true;
+            this.comboBox13.Items.AddRange(new object[] {
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "J",
+            "Q",
+            "K",
+            "A"});
+            this.comboBox13.Location = new System.Drawing.Point(6, 31);
+            this.comboBox13.Name = "comboBox13";
+            this.comboBox13.Size = new System.Drawing.Size(37, 21);
+            this.comboBox13.TabIndex = 6;
+            this.comboBox13.Text = "A";
+            // 
+            // comboBox14
+            // 
+            this.comboBox14.FormattingEnabled = true;
+            this.comboBox14.Items.AddRange(new object[] {
+            "Clubs",
+            "Diamonds",
+            "Hearts",
+            "Spades"});
+            this.comboBox14.Location = new System.Drawing.Point(6, 58);
+            this.comboBox14.Name = "comboBox14";
+            this.comboBox14.Size = new System.Drawing.Size(71, 21);
+            this.comboBox14.TabIndex = 7;
+            this.comboBox14.Text = "Spades";
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.label1);
+            this.groupBox5.Controls.Add(this.txtNumOfOpponents);
+            this.groupBox5.Controls.Add(this.cmdOppPlus);
+            this.groupBox5.Controls.Add(this.cmdOppMinus);
+            this.groupBox5.Location = new System.Drawing.Point(12, 222);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(254, 173);
+            this.groupBox5.TabIndex = 4;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Game Info";
+            // 
+            // cmdOppMinus
+            // 
+            this.cmdOppMinus.Location = new System.Drawing.Point(12, 60);
+            this.cmdOppMinus.Name = "cmdOppMinus";
+            this.cmdOppMinus.Size = new System.Drawing.Size(38, 24);
+            this.cmdOppMinus.TabIndex = 0;
+            this.cmdOppMinus.Text = "-";
+            this.cmdOppMinus.UseVisualStyleBackColor = true;
+            this.cmdOppMinus.Click += new System.EventHandler(this.cmdOppMinus_Click);
+            // 
+            // cmdOppPlus
+            // 
+            this.cmdOppPlus.Location = new System.Drawing.Point(55, 60);
+            this.cmdOppPlus.Name = "cmdOppPlus";
+            this.cmdOppPlus.Size = new System.Drawing.Size(38, 24);
+            this.cmdOppPlus.TabIndex = 1;
+            this.cmdOppPlus.Text = "+";
+            this.cmdOppPlus.UseVisualStyleBackColor = true;
+            // 
+            // txtNumOfOpponents
+            // 
+            this.txtNumOfOpponents.Location = new System.Drawing.Point(12, 35);
+            this.txtNumOfOpponents.Name = "txtNumOfOpponents";
+            this.txtNumOfOpponents.Size = new System.Drawing.Size(81, 20);
+            this.txtNumOfOpponents.TabIndex = 2;
+            this.txtNumOfOpponents.Text = "1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "# of Opponents";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(633, 448);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox comboBox9;
+        private System.Windows.Forms.ComboBox comboBox10;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.ComboBox comboBox8;
+        private System.Windows.Forms.ComboBox comboBox6;
+        private System.Windows.Forms.ComboBox comboBox7;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ComboBox comboBox11;
+        private System.Windows.Forms.ComboBox comboBox12;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.ComboBox comboBox13;
+        private System.Windows.Forms.ComboBox comboBox14;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumOfOpponents;
+        private System.Windows.Forms.Button cmdOppPlus;
+        private System.Windows.Forms.Button cmdOppMinus;
     }
 }
 
