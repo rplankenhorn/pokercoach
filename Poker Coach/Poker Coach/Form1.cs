607b5c91dc573ede4bf8f80731b1e72554af9fe6
rplankenhorn <rplankenhorn@c5cdc586-8c4b-11dd-8ab4-67102e0efeef>
2008-10-06 05:11:31 +0000

    Added more GUI functionality:     -Hole card, flop, turn, and river input.

--- Poker Coach/Poker Coach/Form1.cs
+++ Poker Coach/Poker Coach/Form1.cs
@@ -6,27 +6,114 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Collections;
 
 namespace Poker_Coach
 {
     public partial class Form1 : Form
     {
+        //List to store hole cards
+        public List<Card> holecards;
+
+        //List to store community cards (Flop, turn, river)
+        public List<Card> community;
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void cmdUpdate_Click(object sender, EventArgs e)
+        private void cmdFlopUpdate_Click(object sender, EventArgs e)
         {
-            CARDVALUE value = (CARDVALUE)Enum.Parse(typeof(CARDVALUE), cbbxCardValue1.SelectedItem.ToString(), true);
-            SUITS suit = (SUITS)Enum.Parse(typeof(SUITS), cbbxCardSuit1.SelectedItem.ToString(), true);
-            Card card1 = new Card(suit,value);
-            value = (CARDVALUE)Enum.Parse(typeof(CARDVALUE), cbbxCardValue2.SelectedItem.ToString(), true);
-            suit = (SUITS)Enum.Parse(typeof(SUITS), cbbxCardSuit2.SelectedItem.ToString(), true);
-            Card card2 = new Card(suit, value);
-            txtStatus.Text = card1.ToString();
-            txtStatus.Text += " and " + card2.ToString();
-            //Colour c = (Colour) Enum.Parse(typeof(Colour), "Red", true);
+            if (cbbxFlopCard1Value.SelectedIndex > -1 && cbbxFlopCard1Suit.SelectedIndex > -1 &&
+                cbbxFlopCard2Value.SelectedIndex > -1 && cbbxFlopCard2Suit.SelectedIndex > -1 &&
+                cbbxFlopCard3Value.SelectedIndex > -1 && cbbxFlopCard3Suit.SelectedIndex > -1)
+            {
+                community = new List<Card>(5);
+
+                CARDVALUE value = (CARDVALUE)Enum.Parse(typeof(CARDVALUE), cbbxFlopCard1Value.SelectedItem.ToString(), true);
+                SUITS suit = (SUITS)Enum.Parse(typeof(SUITS), cbbxFlopCard1Suit.SelectedItem.ToString(), true);
+
+                community.Add(new Card(suit, value));
+
+                value = (CARDVALUE)Enum.Parse(typeof(CARDVALUE), cbbxFlopCard2Value.SelectedItem.ToString(), true);
+                suit = (SUITS)Enum.Parse(typeof(SUITS), cbbxFlopCard2Suit.SelectedItem.ToString(), true);
+
+                community.Add(new Card(suit, value));
+
+                value = (CARDVALUE)Enum.Parse(typeof(CARDVALUE), cbbxFlopCard3Value.SelectedItem.ToString(), true);
+                suit = (SUITS)Enum.Parse(typeof(SUITS), cbbxFlopCard3Suit.SelectedItem.ToString(), true);
+
+                community.Add(new Card(suit, value));
+
+                txtStatus.Text += "\r\n\r\nFlop is: " + community[0].ToString() + ", " + community[1].ToString() +
+                    ", and " + community[2].ToString();
+            }
+        }
+
+        private void cmdHoleUpdate_Click(object sender, EventArgs e)
+        {
+            //Makes sure something is selected before updating.
+            if (cbbxCardSuit1.SelectedIndex > -1 && cbbxCardSuit2.SelectedIndex > -1 &&
+                cbbxCardValue1.SelectedIndex > -1 && cbbxCardValue2.SelectedIndex > -1)
+            {
+                //Initialize List
+                holecards = new List<Card>(2);
+
+                //Convert string to ENUM
+                CARDVALUE value = (CARDVALUE)Enum.Parse(typeof(CARDVALUE), cbbxCardValue1.SelectedItem.ToString(), true);
+                SUITS suit = (SUITS)Enum.Parse(typeof(SUITS), cbbxCardSuit1.SelectedItem.ToString(), true);
+
+                //Create first hole card
+                Card card1 = new Card(suit, value);
+
+                //Convert string to ENUM
+                value = (CARDVALUE)Enum.Parse(typeof(CARDVALUE), cbbxCardValue2.SelectedItem.ToString(), true);
+                suit = (SUITS)Enum.Parse(typeof(SUITS), cbbxCardSuit2.SelectedItem.ToString(), true);
+
+                //Create second hole card
+                Card card2 = new Card(suit, value);
+
+                //Add hole cards to List
+                holecards.Add(card1);
+                holecards.Add(card2);
+
+                //Output to status
+                txtStatus.Text = holecards[0].ToString();
+                txtStatus.Text += " and " + holecards[1].ToString();
+            }
+        }
+
+        private void cmdTurnUpdate_Click(object sender, EventArgs e)
+        {
+            if (cbbxTurnCardSuit.SelectedIndex > -1 && cbbxTurnCardValue.SelectedIndex > -1 &&
+                community != null && community.Count == 3)
+            {
+                CARDVALUE value = (CARDVALUE)Enum.Parse(typeof(CARDVALUE), cbbxTurnCardValue.SelectedItem.ToString(), true);
+                SUITS suit = (SUITS)Enum.Parse(typeof(SUITS), cbbxTurnCardSuit.SelectedItem.ToString(), true);
+
+                Card turn = new Card(suit, value);
+
+                community.Add(turn);
+
+                txtStatus.Text = "Turn Updated: " + turn.ToString();
+            }
+        }
+
+        private void cmdRiverUpdate_Click(object sender, EventArgs e)
+        {
+            if (cbbxRiverCardSuit.SelectedIndex > -1 && cbbxRiverCardValue.SelectedIndex > -1 &&
+                community != null)
+            {
+                CARDVALUE value = (CARDVALUE)Enum.Parse(typeof(CARDVALUE), cbbxRiverCardValue.SelectedItem.ToString(), true);
+                SUITS suit = (SUITS)Enum.Parse(typeof(SUITS), cbbxRiverCardSuit.SelectedItem.ToString(), true);
+
+                Card river = new Card(suit, value);
+
+                community.Add(river);
+
+                txtStatus.Text = "River Updated: " + river.ToString();
+            }
         }
     }
 }
