1939bcbc0c7417f04e8ac961d91ca0a259f28e22
sprtnlayofclay <sprtnlayofclay@c5cdc586-8c4b-11dd-8ab4-67102e0efeef>
2008-10-29 00:42:07 +0000

    Changes made: =>Added Coach Advice =>Added function
    Notes: Still need to add stack and betting values.

--- trunk/Poker Coach/Poker Coach/Coach.cs
+++ trunk/Poker Coach/Poker Coach/Coach.cs
@@ -8,9 +8,47 @@ namespace Poker_Coach
     public class Coach
     {
         private int risk;
-        private double currentPot;
-        private double myStack;
-        private double cost2Play;
+
+
+        public Coach(int ourRisk)
+        {
+            risk = ourRisk;
+        }
+
+        public double preFlopCalc(double percentWin, double ourStack, double potSize, int ourPosition, double costToPlay, int totalPlayers)
+        {
+            double value;
+
+            value = (((percentWin - 7.28) / (39.05 - 7.28)) * 16 * risk) + ((potSize / (potSize + costToPlay)) * risk * 8) + (((double)ourPosition / (double)totalPlayers) * 12);
+
+            return value;
+
+
+        }
+
+        public string preFlopDecision(double value, double costToPlay)
+        {
+            if (value < 21)
+            {
+                if (costToPlay == 0)
+                {
+                    return "Let's see a flop. (Check)\r\n";
+                }
+                else
+                {
+                    return "Fold that garbage\r\n";
+                }
+            }
+            else if (value >= 24)
+            {
+                return "Raise\r\n";
+            }
+            else
+            {
+                return "A simple call will work here\r\n";
+            }
+        }
+
 
 
 
