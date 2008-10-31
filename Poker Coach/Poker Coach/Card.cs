498c1703c4eef7f0bcb0bdb74f883032726e0fd4
rplankenhorn <rplankenhorn@c5cdc586-8c4b-11dd-8ab4-67102e0efeef>
2008-10-31 00:57:03 +0000

    Now determines if quads, full house, trips, two pair, and if a pair were made on the flop.
    Added minor utility functions.

--- Poker Coach/Poker Coach/Card.cs
+++ Poker Coach/Poker Coach/Card.cs
@@ -105,6 +105,154 @@ namespace Poker_Coach
                 return false;
             }
         }
+
+        public string formatCardValue()
+        {
+            if (this.getValueInt() == 11)
+            {
+                return "Jack";
+            }
+            else if (this.getValueInt() == 12)
+            {
+                return "Queen";
+            }
+            else if (this.getValueInt() == 13)
+            {
+                return "King";
+            }
+            else if (this.getValueInt() == 14 || this.getValueInt() == 1)
+            {
+                return "Ace";
+            }
+            else if (this.getValueInt() == 2)
+            {
+                return "Two";
+            }
+            else if (this.getValueInt() == 3)
+            {
+                return "Three";
+            }
+            else if (this.getValueInt() == 4)
+            {
+                return "Four";
+            }
+            else if (this.getValueInt() == 5)
+            {
+                return "Five";
+            }
+            else if (this.getValueInt() == 6)
+            {
+                return "Six";
+            }
+            else if (this.getValueInt() == 7)
+            {
+                return "Seven";
+            }
+            else if (this.getValueInt() == 8)
+            {
+                return "Eight";
+            }
+            else if (this.getValueInt() == 9)
+            {
+                return "Nine";
+            }
+            else if (this.getValueInt() == 10)
+            {
+                return "Ten";
+            }
+            else
+            {
+                return this.getValue().ToString();
+            }
+        }
+
+        public string formatCardValuePlural()
+        {
+            if (this.getValueInt() == 11)
+            {
+                return "Jacks";
+            }
+            else if (this.getValueInt() == 12)
+            {
+                return "Queens";
+            }
+            else if (this.getValueInt() == 13)
+            {
+                return "Kings";
+            }
+            else if (this.getValueInt() == 14 || this.getValueInt() == 1)
+            {
+                return "Aces";
+            }
+            else if (this.getValueInt() == 2)
+            {
+                return "Twos";
+            }
+            else if (this.getValueInt() == 3)
+            {
+                return "Threes";
+            }
+            else if (this.getValueInt() == 4)
+            {
+                return "Fours";
+            }
+            else if (this.getValueInt() == 5)
+            {
+                return "Fives";
+            }
+            else if (this.getValueInt() == 6)
+            {
+                return "Sixes";
+            }
+            else if (this.getValueInt() == 7)
+            {
+                return "Sevens";
+            }
+            else if (this.getValueInt() == 8)
+            {
+                return "Eights";
+            }
+            else if (this.getValueInt() == 9)
+            {
+                return "Nines";
+            }
+            else if (this.getValueInt() == 10)
+            {
+                return "Tens";
+            }
+            else
+            {
+                return this.getValue().ToString();
+            }
+        }
+
+        public Card maxOfThree(Card two, Card three)
+        {
+            if (this.getValueInt() == 1)
+            {
+                return this;
+            }
+            else if (two.getValueInt() == 1)
+            {
+                return two;
+            }
+            else if (three.getValueInt() == 1)
+            {
+                return three;
+            }
+            else if (this.getValueInt() > two.getValueInt() && this.getValueInt() > three.getValueInt())
+            {
+                return this;
+            }
+            else if (two.getValueInt() > this.getValueInt() && two.getValueInt() > three.getValueInt())
+            {
+                return two;
+            }
+            else
+            {
+                return three;
+            }
+        }
     }
 }
 
--- Poker Coach/Poker Coach/FlopOdds.cs
+++ Poker Coach/Poker Coach/FlopOdds.cs
@@ -16,6 +16,10 @@ namespace Poker_Coach
         {
             List<Card> sorted = sortHoleAndCommunity(holecards, community);
             String r = "";
+            Boolean fullhouse = false;
+            Boolean pair = false;
+            Boolean trips = false;
+            Boolean quads = false;
 
             int[] pairIndexStore = new int[5];
 
@@ -34,24 +38,176 @@ namespace Poker_Coach
                 }
             }
 
-            for (int k = 0; k < 5; k++)
+            /**
+             * Since the list is sorted, there are only two possible full house configurations.
+             * AAAAX, X2222
+             */
+            if (sorted[0].getValueInt() == sorted[1].getValueInt() && sorted[1].getValueInt() == sorted[2].getValueInt()
+                && sorted[2].getValueInt() == sorted[3].getValueInt())
             {
-                switch(pairIndexStore[k])
+                return "Quad " + sorted[0].formatCardValuePlural();
+            }
+
+            if (sorted[1].getValueInt() == sorted[2].getValueInt() && sorted[2].getValueInt() == sorted[3].getValueInt()
+                && sorted[3].getValueInt() == sorted[4].getValueInt())
+            {
+                return "Quad " + sorted[1].formatCardValuePlural();
+            }
+
+            /**
+             * Since the list is sorted, there are only two possible full house configurations.
+             */ 
+
+            /**
+             * Example: AAAKK
+             */ 
+            if (sorted[0].getValueInt() == sorted[1].getValueInt() && sorted[0].getValueInt() == sorted[2].getValueInt()
+                && sorted[3].getValueInt() == sorted[4].getValueInt())
+            {
+                return sorted[0].formatCardValuePlural() + " full of " + sorted[3].formatCardValuePlural();
+            }
+
+            /**
+             * Example: KK222
+             */ 
+            if (sorted[0].getValueInt() == sorted[1].getValueInt() && sorted[2].getValueInt() == sorted[3].getValueInt()
+                && sorted[3].getValueInt() == sorted[4].getValueInt())
+            {
+                return sorted[3].formatCardValuePlural() + " full of " + sorted[0].formatCardValuePlural();
+            }
+
+            /**
+             * Again, since it is sorted, only 3 possible trip configurations.
+             * AAAXY, XJJJY, XY222.
+             */
+
+            if (sorted[0].getValueInt() == sorted[1].getValueInt() && sorted[0].getValueInt() == sorted[2].getValueInt()
+                && sorted[3].getValueInt() != sorted[4].getValueInt())
+            {
+                if (sorted[3].getValueInt() == 1 || sorted[4].getValueInt() == 1)
                 {
-                    case 1:
-                        r += " a Pair of " + sorted[k].getValue().ToString();
-                        break;
-                    case 2:
-                        r += " Trip " + sorted[k].getValue().ToString();
-                        break;
-                    case 3:
-                        r += " Quad " + sorted[k].getValue().ToString();
-                        break;
-                  
+                    return "Trip " + sorted[0].formatCardValuePlural() + " with an Ace Kicker";
+                }
+                else if (sorted[3].getValueInt() > sorted[4].getValueInt())
+                {
+                    return "Trip " + sorted[0].formatCardValuePlural() + " with " + sorted[3].formatCardValue() + " Kicker";
+                }
+                else
+                {
+                    return "Trip " + sorted[0].formatCardValuePlural() + " with " + sorted[4].formatCardValue() + " Kicker";
                 }
             }
 
-            return r;
+            if (sorted[1].getValueInt() == sorted[2].getValueInt() && sorted[1].getValueInt() == sorted[3].getValueInt()
+                && sorted[0].getValueInt() != sorted[4].getValueInt())
+            {
+                if (sorted[0].getValueInt() == 1 || sorted[4].getValueInt() == 1)
+                {
+                    return "Trip " + sorted[1].formatCardValuePlural() + " with an Ace Kicker";
+                }
+                else if (sorted[0].getValueInt() > sorted[4].getValueInt())
+                {
+                    return "Trip " + sorted[1].formatCardValuePlural() + " with " + sorted[0].formatCardValue() + " Kicker";
+                }
+                else
+                {
+                    return "Trip " + sorted[1].formatCardValuePlural() + " with " + sorted[4].formatCardValue() + " Kicker";
+                }
+            }
+
+            if (sorted[2].getValueInt() == sorted[3].getValueInt() && sorted[2].getValueInt() == sorted[4].getValueInt()
+                && sorted[0].getValueInt() != sorted[1].getValueInt())
+            {
+                if (sorted[0].getValueInt() == 1 || sorted[1].getValueInt() == 1)
+                {
+                    return "Trip " + sorted[2].formatCardValuePlural() + " with an Ace Kicker";
+                }
+                else if (sorted[0].getValueInt() > sorted[1].getValueInt())
+                {
+                    return "Trip " + sorted[2].formatCardValuePlural() + " with " + sorted[0].formatCardValue() + " Kicker";
+                }
+                else
+                {
+                    return "Trip " + sorted[2].formatCardValuePlural() + " with " + sorted[1].formatCardValue() + " Kicker";
+                }
+            }
+
+            /**
+             * Only three possible two pair configurations
+             * XXYYZ, ZXXYY, XXZYY
+             */
+
+            if (sorted[0].getValueInt() == sorted[1].getValueInt() && sorted[2].getValueInt() == sorted[3].getValueInt())
+            {
+                if (sorted[0].getValueInt() > sorted[2].getValueInt())
+                {
+                    return "Two pair of " + sorted[0].formatCardValuePlural() + " and " + sorted[2].formatCardValuePlural()
+                        + " with " + sorted[4].formatCardValue() + " Kicker";
+                }
+                else
+                {
+                    return "Two pair of " + sorted[2].formatCardValuePlural() + " and " + sorted[0].formatCardValuePlural()
+                        + " with " + sorted[4].formatCardValue() + " Kicker";
+                }
+            }
+
+            if (sorted[1].getValueInt() == sorted[2].getValueInt() && sorted[3].getValueInt() == sorted[4].getValueInt())
+            {
+                if (sorted[1].getValueInt() > sorted[3].getValueInt())
+                {
+                    return "Two pair of " + sorted[1].formatCardValuePlural() + " and " + sorted[3].formatCardValuePlural()
+                        + " with " + sorted[0].formatCardValue() + " Kicker";
+                }
+                else
+                {
+                    return "Two pair of " + sorted[3].formatCardValuePlural() + " and " + sorted[1].formatCardValuePlural()
+                        + " with " + sorted[0].formatCardValue() + " Kicker";
+                }
+            }
+
+            if (sorted[0].getValueInt() == sorted[1].getValueInt() && sorted[3].getValueInt() == sorted[4].getValueInt())
+            {
+                if (sorted[1].getValueInt() > sorted[3].getValueInt())
+                {
+                    return "Two pair of " + sorted[1].formatCardValuePlural() + " and " + sorted[3].formatCardValuePlural()
+                        + " with " + sorted[2].formatCardValue() + " Kicker";
+                }
+                else
+                {
+                    return "Two pair of " + sorted[3].formatCardValuePlural() + " and " + sorted[1].formatCardValuePlural()
+                        + " with " + sorted[2].formatCardValue() + " Kicker";
+                }
+            }
+
+            /*
+             * Again, since list is sorted, there are only four possible positions for pairs.
+             * AAXYZ, XKKYZ, XYJJZ, XYZ22
+             */
+            if (sorted[0].getValueInt() == sorted[1].getValueInt())
+            {
+                return "A pair of " + sorted[0].formatCardValuePlural() + " with a " + (sorted[2].maxOfThree(sorted[3], 
+                    sorted[4])).formatCardValue() + " kicker";
+            }
+
+            if (sorted[1].getValueInt() == sorted[2].getValueInt())
+            {
+                return "A pair of " + sorted[1].formatCardValuePlural() + " with a " + (sorted[0].maxOfThree(sorted[3],
+                    sorted[4])).formatCardValue() + " kicker";
+            }
+
+            if (sorted[2].getValueInt() == sorted[3].getValueInt())
+            {
+                return "A pair of " + sorted[2].formatCardValuePlural() + " with a " + (sorted[0].maxOfThree(sorted[1],
+                    sorted[4])).formatCardValue() + " kicker";
+            }
+
+            if (sorted[3].getValueInt() == sorted[4].getValueInt())
+            {
+                return "A pair of " + sorted[3].formatCardValuePlural() + " with a " + (sorted[0].maxOfThree(sorted[1],
+                    sorted[2])).formatCardValue() + " kicker";
+            }
+
+            return null;
         }
 
         public List<Card> sortHoleAndCommunity(List<Card> holecards, List<Card> community)
